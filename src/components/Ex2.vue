bd800c60cb96075973b4cd1b6a5ec873b6f2262c
shreelakham <shreelakham.2024@smu.edu.sg>
2025-11-01 09:06:33 +0000

    redone qn 2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,10 +2,24 @@
     export default { 
         // Add code in appropriate places
         data() {
+
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                Name : ""
             }
             
+        },
+        methods :{
+            del(idx){
+                console.log(idx)
+                return this.items.splice(idx, 1)
+
+            },
+
+            AddItem(){
+                return this.items.push(this.Name)
+                
+            }
         }
     }
 </script>
@@ -14,5 +28,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+        <li v-for = "item, idx in items">{{ item }}<button v-on:click="del(idx)">Delete</button></li>
+     </ul>
+
+     <div>
+        <input type="text" v-model="Name"></input>
+        <button v-on:click="AddItem()">Add!</button>
+     </div>
    
 </template>
